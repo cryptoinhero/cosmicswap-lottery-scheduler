d079368a2cadfc8eae3eb40c24f71036d716c3ae
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2021-10-12 21:48:11 +0800

    update: lottery start time once a day

--- utils/index.ts
+++ utils/index.ts
@@ -48,16 +48,16 @@ export const getEndTime = (): number => {
   // Get meridiem (AM/PM), based on current UTC Date.
   const meridiem = now.format("A");
   if (meridiem === "AM") {
-    // We are in the morning (ante-meridiem), next lottery is at 12:00 PM (noon).
+   // We are in the afternoon (post-meridiem), next lottery is at 12:00 AM (midnight).
     return moment(`${now.format("MM DD YYYY")} 00:00:00 +0000`, "MM DD YYYY HH:mm:ss Z", true)
-      .add(12, "hours")
+      .add(24, "hours")
       .startOf("hour")
       .utc()
       .unix();
   } else if (meridiem === "PM") {
     // We are in the afternoon (post-meridiem), next lottery is at 12:00 AM (midnight).
-    return moment(`${now.format("MM DD YYYY")} 12:00:00 +0000`, "MM DD YYYY HH:mm:ss Z", true)
-      .add(12, "hours")
+    return moment(`${now.format("MM DD YYYY")} 00:00:00 +0000`, "MM DD YYYY HH:mm:ss Z", true)
+      .add(24, "hours")
       .startOf("hour")
       .utc()
       .unix();
